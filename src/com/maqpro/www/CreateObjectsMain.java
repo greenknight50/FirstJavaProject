4cea71dffce298f9e87a43651d0d87f09fb9474d
Ashraf Mohammed <ashraf.mohammed@redmorph.com>
2017-11-03 08:48:05 +0530

    Craeted object using cloneable interface

--- src/com/maqpro/www/CreateObjectsMain.java
+++ src/com/maqpro/www/CreateObjectsMain.java
@@ -1,17 +1,21 @@
 package com.maqpro.www;
 
-public class CreateObjectsMain {
+public class CreateObjectsMain implements Cloneable {
+	
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
 
 	private String className = "CreateObjectsMain";
 
-	public static void main(String[] args) {
-		try {
-			Class className = Class.forName("com.maqpro.www.CreateObjectsMain");
-			CreateObjectsMain obj = (CreateObjectsMain) className.newInstance();
-			System.out.println("Object created for class" + obj.className);
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
-			ex.printStackTrace();
-		}
+	public static void main(String[] args) throws CloneNotSupportedException {
+		
+			CreateObjectsMain object1 = new CreateObjectsMain();
+			object1.className = "Object1Name";
+			CreateObjectsMain object2 = (CreateObjectsMain)object1.clone();
+			System.out.println("Classname from object2 instance var: " +object2.className);
+	
 	}
 
 }
