128ab0190eb7e89805f5e8247108a3e0098b599e
Ashraf Mohammed <ashraf.mohammed@redmorph.com>
2017-11-03 08:25:36 +0530

    New file for object creation topic

---
+++ src/com/maqpro/www/CreateObjectsMain.java
@@ -0,0 +1,14 @@
+package com.maqpro.www;
+
+public class CreateObjectsMain {
+	
+	private String className = "CreateObjectsMain";
+
+	public static void main(String[] args) {
+		
+		CreateObjectsMain object = new CreateObjectsMain();
+		System.out.println("Class name: " + object.className);
+
+	}
+
+}
