b51f66ad62be7142db5692daa8d2e696965f0ecc
Ashraf Mohammed <ashraf.mohammed@redmorph.com>
2017-11-05 08:12:30 +0530

    Static binding, static polym.., overloading

--- src/com/maqpro/www/FirstMain.java
+++ src/com/maqpro/www/FirstMain.java
@@ -7,40 +7,68 @@ package com.maqpro.www;
  *
  */
 public class FirstMain {
-	
+
 	protected String userName;
-	String emailId;
+	protected long userId;
+
+	public FirstMain() {
+		
+	}
+
+	public FirstMain(String userName) {
+		this.userName = userName;
+		this.userId = 100;
+	}
+
+	public FirstMain(long userId) {
+		this.userId = userId;
+		this.userName = "DefaultUserName";
+	}
+
+	public FirstMain(String userName, long userId) {
+		this.userName = userName;
+		this.userId = userId;
+	}
 
 	/**
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
-		/*
-		 * below statement will print hello world onto the console. this line will not
-		 * be treated as part of above comment
-		 */
-		System.out.println("\"Hello World!\"");
-		FirstMain firstMain = new FirstMain();
-		String emailAddress = "java.ashraf@gmail.com";
-		for (int i =0; i< 10; i++) {
-			System.out.print(i + " ");
-		}
-		
-		String str = emailAddress;
+		/*FirstMain firstMain1 = new FirstMain();
+		System.out.println("userName: " + firstMain1.userName + ", userId: " + firstMain1.userId);*/
+		FirstMain firstMain2 = new FirstMain("Ashraf", 88);
+		System.out.println("userName: " + firstMain2.userName + ", userId: " + firstMain2.userId);
+		FirstMain firstMain3 = new FirstMain(5001);
+		System.out.println("userName: " + firstMain3.userName + ", userId: " + firstMain3.userId);
+		FirstMain firstMain4 = new FirstMain("Arhaan", 7000);
+		System.out.println("userName: " + firstMain4.userName + ", userId: " + firstMain4.userId);
 	}
-	//String str1 = emailAddress;
-	
+
 	/**
 	 * This method will fetch user details based on the userId and email provided
-	 * @param userId userId of the user whose details are to be fetched
-	 * @param emailAddress emailId of the user whose details are to be fetched
+	 * 
+	 * @param emailAddress
+	 *            emailId of the user whose details are to be fetched
 	 * @return returns userName and emailAddress as a string.
-	 * @throws NullPointerException this method might throw NullPointerException if not checked properly
+	 * @throws NullPointerException
+	 *             this method might throw NullPointerException if not checked
+	 *             properly
 	 */
-	public String getUserDetails(long userId, String emailAddress) throws NullPointerException {
+	public String getUserDetails(String emailAddress) throws NullPointerException {
 		return "Ashraf, java.ashraf@gmail.com";
 	}
-	
+
+	public String getUserDetails(long userId) {
+		return "" + userId + userId;
+	}
+
+	public long getUserDetails(String emailAddress, long yearOfBirth) {
+		return yearOfBirth;
+	}
+
+	public String getUserDetails(long yearOfBirth, String emailAddress) {
+		return emailAddress + yearOfBirth;
+	}
+
 }
