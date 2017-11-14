91c5ae1ebf1c55959ee55de29fdd944fabf4d4aa
Ashraf Mohammed <ashraf.mohammed@redmorph.com>
2017-11-14 09:03:37 +0530

    While loop and do while loop

--- src/com/maqpro/control/statements/ControlStatementsMain.java
+++ src/com/maqpro/control/statements/ControlStatementsMain.java
@@ -1,17 +1,27 @@
 package com.maqpro.control.statements;
 
-public class ControlStatementsMain {
+import java.util.Scanner;
 
+public class ControlStatementsMain {
 	public static void main(String[] args) {
-		int[] array = {11, 2, 321, 54, 50};
-		for (int i = 0; i < array.length; i++) {
-			System.out.println("array[" + i + "] = " + array[i]);
+		int i = 0;
+		int j = 0;
+		String userPassword;
+		Scanner scanner = new Scanner(System.in);
+
+		while (j < 10) {
+			System.out.println("From while loop, j = " + j++);
 		}
-		System.out.println("Array iteration using for-each loop");
-		for (int i : array) {
-			System.out.println("array[" + i + "] = " + i);
+
+		do {
+			i++;
+			System.out.println("Enter your password: ");
+			userPassword = scanner.next();
+		} while (!"password".equalsIgnoreCase(userPassword) && (i < 3));
+		if (i == 3) {
+			System.out.println("You have enter wrong password for 3 times");
 		}
-		System.out.println("End of for loop and main as well: ");
+		System.out.println("Out of do while loop and end of main method");
 	}
 
 }
