c76397e40757ee0da58cf4d9dc9824b016c96a0c
Ashraf Mohammed <ashraf.mohammed@redmorph.com>
2017-11-25 09:40:06 +0530

    Stack and its behaviour

--- src/com/maqpro/collections/CollectionImplMain.java
+++ src/com/maqpro/collections/CollectionImplMain.java
@@ -1,57 +1,27 @@
 package com.maqpro.collections;
 
-import java.util.Vector;
+import java.util.Stack;
 
 public class CollectionImplMain {
 
 	public static void main(String[] args) {
 
-		Vector<Integer> vector1 = new Vector<Integer>();
-		Vector<Integer> vector2 = new Vector<Integer>(20);
-		Vector<Integer> vector3 = new Vector<Integer>(20, 5);
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(8);
+		stack.push(101);
+		stack.push(1);
+		stack.push(-43);
+		System.out.println("stack search: " + stack.search(-43));
+		System.out.println("stack search: " + stack.search(101));
+		System.out.println("stack pop: " + stack.pop());
+		System.out.println("stack pop: " + stack.pop());
+		System.out.println("stack peek: " +stack.peek());
+		System.out.println("stack peek: " +stack.peek());
+		System.out.println("stack peek: " +stack.peek());
+		System.out.println("stack pop: " + stack.pop());
+		System.out.println("stack pop: " + stack.pop());
 		
-		System.out.println("vector1 size: " + vector1.size() + ", vector1 capacity: " + vector1.capacity());
-		for (int i = 0; i < 10; i++) {
-			vector1.addElement(i);
-		}
-		vector1.add(10);
-		System.out.println(
-				"vector1 size after initial add: " + vector1.size() + ", vector1 capacity: " + vector1.capacity());
-		int size = vector1.size();
-		int capacity = vector1.capacity();
-		for (int i = size; i <= capacity; i++) {
-			//System.out.println("adding element");
-			vector1.addElement(i);
-		}
-		System.out.println("vector1 size after capacity inc 20: " + vector1.size() + ", vector1 capacity: " + vector1.capacity());
-		size = vector1.size();
-		capacity = vector1.capacity();
-		for (int i = size; i <= capacity; i++) {
-			//System.out.println("adding element");
-			vector1.addElement(i);
-		}
-		System.out.println("vector1 size after capacity inc 40: " + vector1.size() + ", vector1 capacity: " + vector1.capacity());
-		vector1.trimToSize();
-		System.out.println(
-				"vector1 size after trimToSize: " + vector1.size() + ", vector1 capacity: " + vector1.capacity());
+		//System.out.println("stack peek: " +stack.peek()); //
 		
-		System.out.println("*************** vector3 with 2 argument****************\n\n");
-		for (int i = 0; i<=20; i++) {
-			//System.out.println();
-			vector3.add(i);
-		}
-		System.out.println("vector3 size is: " + vector3.size() + ", vector3 capacity: " + vector3.capacity());
-		size = vector1.size();
-		capacity = vector1.capacity();
-		for (int i = size; i <= capacity; i++) {
-			//System.out.println("adding element");
-			vector3.addElement(i);
-		}
-		System.out.println("vector3 size after c is: " + vector3.size() + ", vector3 capacity: " + vector3.capacity());
-		
-		Vector<Integer> vector4 = new Vector(vector1);
-		System.out.println("vector4 size: " + vector4.size() + ", capacty: " + vector4.capacity());
-		vector4.add(42);
-		System.out.println("vector4 size: " + vector4.size() + ", capacty: " + vector4.capacity());
 	}
 }
