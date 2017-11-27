b316fd825bd72387eff422fb7a057529a7dd0f34
Ashraf Mohammed <ashraf.mohammed@redmorph.com>
2017-11-27 08:57:24 +0530

    HashSet and its behaviour

--- src/com/maqpro/collections/CollectionImplMain.java
+++ src/com/maqpro/collections/CollectionImplMain.java
@@ -1,27 +1,31 @@
 package com.maqpro.collections;
 
-import java.util.Stack;
+import java.util.HashSet;
 
 public class CollectionImplMain {
 
 	public static void main(String[] args) {
-
-		Stack<Integer> stack = new Stack<Integer>();
-		stack.push(8);
-		stack.push(101);
-		stack.push(1);
-		stack.push(-43);
-		System.out.println("stack search: " + stack.search(-43));
-		System.out.println("stack search: " + stack.search(101));
-		System.out.println("stack pop: " + stack.pop());
-		System.out.println("stack pop: " + stack.pop());
-		System.out.println("stack peek: " +stack.peek());
-		System.out.println("stack peek: " +stack.peek());
-		System.out.println("stack peek: " +stack.peek());
-		System.out.println("stack pop: " + stack.pop());
-		System.out.println("stack pop: " + stack.pop());
 		
-		//System.out.println("stack peek: " +stack.peek()); //
-		
+		HashSet<String> hashSet = new HashSet<String>();
+		hashSet.add("Chicago");
+		hashSet.add("Atlanta");
+		hashSet.add("Hyderabad");
+		hashSet.add("California");
+		System.out.println("Hashset elements: " + hashSet);
+		hashSet.add("Chicago");
+		hashSet.add(null);
+		System.out.println("Hashset elements after adding chicago: " + hashSet);
+		HashSet<Integer> hashSet1 = new HashSet<Integer>(20);
+		HashSet<Integer> hashSet2  = new HashSet<Integer>(20, 0.8f);
+		for(int i = 0; i< 22; i++) {
+			hashSet2.add(i);
+		}
+		System.out.println("Hashset2 : "  + hashSet2);
+		HashSet<Integer> hashSet3 = new HashSet<Integer>(hashSet2);
+		System.out.println("Hashset3: "  + hashSet3);
+		hashSet1.add(8);
+		hashSet1.add(3);
+		hashSet1.add(51);
+		System.out.println(hashSet1);
 	}
 }
