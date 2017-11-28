06cf02bb70467764bb903b5cd57a954af9ac3cdd
Ashraf Mohammed <ashraf.mohammed@redmorph.com>
2017-11-28 15:35:57 +0530

    Iterators

--- src/com/maqpro/collections/CollectionImplMain.java
+++ src/com/maqpro/collections/CollectionImplMain.java
@@ -1,25 +1,51 @@
 package com.maqpro.collections;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.ListIterator;
 import java.util.TreeSet;
 
 public class CollectionImplMain {
 
 	public static void main(String[] args) {
-		
+
 		TreeSet<Integer> ts = new TreeSet<Integer>();
 		ts.add(53);
 		ts.add(1);
 		ts.add(10);
 		ts.add(101);
 		System.out.println("Treeset element: " + ts);
-		TreeSet<Integer> ts1 = new TreeSet<Integer>(Collections.reverseOrder());
-		ts1.add(83);
-		ts1.add(4);
-		ts1.add(10);
-		ts1.add(3);
-		ts1.containsAll(ts);
-		System.out.println("TreeSet in desc: " + ts1);
-		System.out.println("Ts contains: " + ts1.contains(53));
+		Iterator tsIterator = ts.iterator();
+		try {
+			while (tsIterator.hasNext()) {
+				int element = (int) tsIterator.next();
+				if (element == 53) {
+					System.out.println("removing value: " + element);
+					ts.remove(element);
+					// tsIterator.remove();
+				}
+				System.out.println("element: " + element);
+			}
+		} catch (ConcurrentModificationException e) {
+			System.out.println("Inside catch block");
+			//e.printStackTrace();
+		}
+		System.out.println("Treeset element after removing: " + ts);
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(15);
+		list.add(10);
+		list.add(30);
+		list.add(20);
+		System.out.println("current list: " + list);
+		ListIterator listIterator = list.listIterator();
+		while(listIterator.hasNext()) {
+			int value = (int) listIterator.next();
+			if (value == 10) {
+				listIterator.set(55);
+			}
+			System.out.println("value: " + value);
+		}
+		System.out.println("list after adding: " + list);
 	}
 }
