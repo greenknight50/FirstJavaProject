476cc9838be760f413cd81e48ad35b2a66381b07
Ashraf Mohammed <ashraf.mohammed@redmorph.com>
2017-12-06 09:29:36 +0530

    Synchronized keyword

--- src/com/maqpro/collections/CollectionImplMain.java
+++ src/com/maqpro/collections/CollectionImplMain.java
@@ -2,43 +2,37 @@ package com.maqpro.collections;
 
 class CustomThread extends Thread {
 
+	static Long accountBalance = 1000l;
+
 	@Override
 	public void run() {
-		for (int i = 0; i < 200; i++) {
-			System.out.println("i value from thread " + this.getName() + " is: " + i + ", and is it alive: " + this.isAlive() + ", its status: " + this.getState());
-			if (i == 100) {
-				this.yield();
-			}
-			try {
-				this.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		
+		//95 lines
+		synchronized (accountBalance) {
+			accountBalance = accountBalance - 100;
+			System.out.println("Thread name: " + this.getName() + ", i value: " + accountBalance);
 		}
+		//2 lines
 	}
 }
 
 public class CollectionImplMain {
 
 	public static void main(String[] args) throws InterruptedException {
-		System.out.println("Inside main method");
 		CustomThread t1 = new CustomThread();
 		CustomThread t2 = new CustomThread();
 		t1.setName("First");
 		t2.setName("Second");
 		t1.start();
-		/*
-		 * t1.sleep(5000); System.out.println("t1 state: " + t1.getState());
-		 */
 		t2.start();
-		t1.setPriority(Thread.MIN_PRIORITY);
-		t2.setPriority(Thread.MAX_PRIORITY);
-		System.out.println("About to join main thread to the end of t1 thread");
-		t1.join();
-		t1.yield();
-		System.out.println("Resuming main thread");
-		System.out.println("End of main method");
+	}
+	
+	public void printSome() {
+		System.err.println("print now.");
+		
+		
+		
+		//100 lines
 	}
 
 }
